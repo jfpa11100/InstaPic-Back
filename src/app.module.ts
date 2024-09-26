fab8eb46a8de45a399a7a107faa108f555114940
jfpa11100 <felipepala1000@gmail.com>
2024-09-25 20:08:35 -0500

    Creating posts controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PostsController } from './posts.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [PostsController],
+  providers: [],
 })
 export class AppModule {}
